048a24d468dff42f79c7e968c67d201134207bb8
vitoriaraay <vitoria.lobo.melo@escola.pr.gov.br>
2025-08-19 10:34:55 -0300

    Upd

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Abrir o formulário de agendamento
-function abrirFormulario() {
-  document.getElementById("contato").scrollIntoView({ behavior: "smooth" });
-}
 
-// Validação simples do formulário
-document.getElementById("form-agendamento").addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("Agendamento realizado com sucesso! Entraremos em contato em breve.");
-  this.reset();
-});
